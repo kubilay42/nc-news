77b496ce24dfe28cfbf961056019ab8648fcd05e
Kubilay <kubilay42@Kubilays-MacBook-Pro.local>
2024-02-22 09:27:28 +0000

    completed tasks 6 and 7

--- db/app.js
+++ db/app.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const {
   getAllTopics,
   getAllEndpoints,
   getArticleById,
-  getAllArticles
+  getAllArticles,
+  getCommentForArticle,
+  addCommentById
 } = require("../controllers/nc_news.controller");
 
 app.get("/api", getAllEndpoints);
 app.get("/api/topics", getAllTopics);
 app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles", getAllArticles);
+app.get("/api/articles/:article_id/comments", getCommentForArticle)
+app.post("/api/articles/:article_id/comments", addCommentById)
 
 app.all("/*", (req, res, next) => {
   res.status(404).send({ msg: "Not found" });
   next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "23503") {
+    res.status(404).send({ msg: "Not found" });
+  } else {
+    next(err);
+  }
 });
 
 app.use((err, req, res, next) => {
